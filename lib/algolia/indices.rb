6e371576a6deb25ae58a18c3bd0eec82c9c2a4e2
m-nakamura145 <masato.nakamura145@gmail.com>
2018-11-22 08:40:43 +0900

    Remove unnecessary code

--- lib/algolia/indices.rb
+++ lib/algolia/indices.rb
@@ -1,15 +1,5 @@
 require 'algolia/indices/version'
 require 'algolia/indices/cli'
 
-module Algolia
-  module Indices
-    class Importer
-      def initialize(api_key, application_id, index_name, path)
-        @path = path
-        @algolia_client = Client.new(api_key, application_id, index_name)
-      end
-    end
-  end
-end
 
 
